830519edde15d814a9d246a78a72b599ea2881ae
Matt Felsen <mattfelsen@gmail.com>
2018-12-03 14:59:32 -0500

    Add a bunch of test sketches

---
+++ tests/test-4/test-4.ino
@@ -0,0 +1,66 @@
+#include <i2c_t3.h>
+#include <Adafruit_AM2315.h>
+#include <OctoWS2811.h>
+
+/***************************************************
+  This is an example for the AM2315 Humidity + Temp sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout
+  ----> https://www.adafruit.com/products/1293
+
+  These displays use I2C to communicate, 2 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// Connect RED of the AM2315 sensor to 5.0V
+// Connect BLACK to Ground
+// Connect WHITE to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
+// Connect YELLOW to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
+
+Adafruit_AM2315 am2315;
+
+const int ledsPerStrip = 300;
+const int numStrips = 8;
+
+const int numPixels = ledsPerStrip * numStrips;
+DMAMEM int displayMemory[ledsPerStrip * 6];
+int drawingMemory[ledsPerStrip * 6];
+const int config = WS2811_GRB | WS2811_800kHz;
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+
+float temperature;
+float humidity;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+  Serial.println("AM2315 Test!");
+
+  if (! am2315.begin()) {
+    Serial.println("Sensor not found, check wiring & pullups!");
+    while (1);
+  }
+
+  leds.begin();
+  leds.show();
+}
+
+void loop() {
+  temperature = am2315.readTemperature();
+  humidity = am2315.readHumidity();
+
+  Serial.print("Hum: "); Serial.println(temperature);
+  Serial.print("Temp: "); Serial.println(humidity);
+
+  leds.show();
+
+  delay(1000);
+}
